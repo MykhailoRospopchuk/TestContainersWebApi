d3ed5e33879a1615ac64a222c1c919babed7f41d
Mykhailo <rosmisha@gmail.com>
2023-05-19 00:34:14 +0300

    Add migraion Up and down

---
+++ TestContainerWebApi/TestContainerWebApi/Migrations/20230518194808_InitMigration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestContainerWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sql_command = Path.Combine("Migrations/20230518194808_InitMigration.sql");
+            migrationBuilder.Sql(File.ReadAllText(sql_command));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string sql_command = Path.Combine("Migrations/20230518194808_InitMigration_Down.sql");
+            migrationBuilder.Sql(File.ReadAllText(sql_command));
+        }
+    }
+}
---
+++ TestContainerWebApi/TestContainerWebApi/Program.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TestContainerWebApi.db;
+
+namespace TestContainerWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            string connection = builder.Configuration.GetConnectionString("DbConnectionString");
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
