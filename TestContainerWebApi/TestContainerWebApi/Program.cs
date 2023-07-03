4beb26ddb36dad957f4b3736e3e2647a20743a3a
Mykhailo <rosmisha@gmail.com>
2023-07-03 23:20:04 +0300

    before implement authentification and authorization

--- TestContainerWebApi/TestContainerWebApi/Program.cs
+++ TestContainerWebApi/TestContainerWebApi/Program.cs
@@ -14,7 +14,6 @@ namespace TestContainerWebApi
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
